5359c89edabe350df56d259fb5733bd7889a63f3
bughunt88 <bughunt@naver.com>
2021-01-29 16:14:38 +0900

    2021-01-29 study

---
+++ ml/m21_FI_test4_boston.py
@@ -0,0 +1,78 @@
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+#1. 데이터
+dataset = load_boston()
+x_train,x_test,y_train,y_test = train_test_split(dataset.data,dataset.target,train_size = 0.8, random_state = 33)
+
+#2. 모델
+model = DecisionTreeRegressor(max_depth = 4)
+
+#3. 훈련
+model.fit(x_train, y_train)
+
+#4. 평가 예측
+acc = model.score(x_test,y_test)
+
+print(model.feature_importances_)
+print('acc : ', acc)
+
+
+'''
+def plot_feature_importances_dataset(model):
+    n_features = dataset.data.shape[1]
+    plt.barh(np.arange(n_features), model.feature_importances_, align = 'center')
+    plt.yticks(np.arange(n_features), dataset.feature_names)
+    plt.xlabel("Feature Importances")
+    plt.ylabel("Features")
+    plt.ylim(-1, n_features)
+
+plot_feature_importances_dataset(model)
+plt.show()
+'''
+
+
+print("############################################")
+
+
+# 위에 돌린 모델에서 feature_importances_에 값이 있는 것들만 추가 함 
+
+fi = model.feature_importances_
+new_data = []
+feature = []
+for i in range(len(fi)):
+    if fi[i] != 0:
+        new_data.append(dataset.data[:,i])
+        feature.append(dataset.feature_names[i])
+
+new_data = np.array(new_data)
+new_data = np.transpose(new_data)
+x_train,x_test,y_train,y_test = train_test_split(new_data,dataset.target,train_size = 0.8, random_state = 33)
+
+#2. 모델
+model = DecisionTreeClassifier(max_depth = 4)
+
+#3. 훈련
+model.fit(x_train, y_train)
+
+#4. 평가 예측
+acc = model.score(x_test,y_test)
+
+print(model.feature_importances_)
+print('acc : ', acc)
+
+####### dataset >> new_data 로 바꾸고 featurename 부분을 feature 리스트로 바꿔줌!!!
+def plot_feature_importances_dataset(model):
+    n_features = new_data.shape[1]
+    plt.barh(np.arange(n_features), model.feature_importances_, align = 'center')
+    plt.yticks(np.arange(n_features), feature)
+    plt.xlabel("Feature Importances")
+    plt.ylabel("Features")
+    plt.ylim(-1, n_features)
+
+plot_feature_importances_dataset(model)
+plt.show()
