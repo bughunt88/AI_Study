338b9cd7d2ad9abf25a42db53dca88c5eff4e23a
bughunt88 <bughunt@naver.com>
2021-02-16 12:03:59 +0900

    2021-02-16

---
+++ project/project_data_save.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+import os
+from scipy.io import wavfile
+from collections import defaultdict, Counter
+from scipy import signal
+import numpy as np
+import librosa
+import sklearn
+import random
+from unicodedata import normalize
+from keras.layers import Dense
+from keras import Input
+from keras.engine import Model
+from keras.utils import to_categorical
+from keras.layers import Dense, TimeDistributed, Dropout, Bidirectional, GRU, BatchNormalization, Activation, LeakyReLU, LSTM, Flatten, RepeatVector, Permute, Multiply, Conv2D, MaxPooling2D
+
+import warnings
+
+warnings.filterwarnings('ignore')
+
+trainset = []
+testset = []
+
+# 모든 음성파일의 길이가 같도록 후위에 padding 처리
+pad1d = lambda a, i: a[0: i] if a.shape[0] > i else np.hstack((a, np.zeros(i-a.shape[0])))
+pad2d = lambda a, i: a[:, 0:i] if a.shape[1] > i else np.hstack((a, np.zeros((a.shape[0], i-a.shape[1]))))
+
+TRAIN_DATA_DIR = '../data/project/train/'
+TEST_DATA_DIR = '../data/project/test/'
+
+DIR_List = ['angry/','nomal/','sad/']
+
+for index, d_list in enumerate(DIR_List):
+    # 0 - 화남 
+    for filename in os.listdir(TRAIN_DATA_DIR+d_list):
+        filename = normalize('NFC', filename)
+        try:
+            wav, sr = librosa.load(TRAIN_DATA_DIR + d_list + filename)
+            
+            mfcc = librosa.feature.mfcc(wav, sr=16000, n_mfcc=100, n_fft=400, hop_length=160)
+            mfcc = sklearn.preprocessing.scale(mfcc, axis=1)
+            padded_mfcc = pad2d(mfcc, 40)
+
+            trainset.append((padded_mfcc, index))
+
+        except Exception as e:
+            print(filename, e)
+            raise
+
+for index, d_list in enumerate(DIR_List):
+    # 0 - 화남 
+    for filename in os.listdir(TEST_DATA_DIR+d_list):
+        filename = normalize('NFC', filename)
+        try:
+            wav, sr = librosa.load(TEST_DATA_DIR + d_list + filename)
+            
+            mfcc = librosa.feature.mfcc(wav, sr=16000, n_mfcc=100, n_fft=400, hop_length=160)
+            mfcc = sklearn.preprocessing.scale(mfcc, axis=1)
+            padded_mfcc = pad2d(mfcc, 40)
+
+            testset.append((padded_mfcc, index))
+
+        except Exception as e:
+            print(filename, e)
+            raise
+
+
+
+# 학습 데이터를 무작위로 섞는다.
+random.shuffle(trainset)
+random.shuffle(testset)
+
+np.save('../data/project/train_data.npy', arr=trainset)
+np.save('../data/project/test_data.npy', arr=testset)
+
---
+++ project/project_model.py
@@ -0,0 +1,133 @@
+import matplotlib.pyplot as plt
+import os
+from scipy.io import wavfile
+from collections import defaultdict, Counter
+from scipy import signal
+import numpy as np
+import librosa
+import sklearn
+import random
+from unicodedata import normalize
+from keras.layers import Dense
+from keras import Input
+from keras.engine import Model
+from keras.utils import to_categorical
+from keras.layers import Dense, TimeDistributed, Dropout, Bidirectional, GRU, BatchNormalization, Activation, LeakyReLU, LSTM, Flatten, RepeatVector, Permute, Multiply, Conv2D, MaxPooling2D
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import EarlyStopping,ReduceLROnPlateau
+
+import warnings
+
+warnings.filterwarnings('ignore')
+
+trainset = np.load('../data/project/data/train_data.npy',allow_pickle=True)
+testset = np.load('../data/project/data/test_data.npy',allow_pickle=True)
+
+# split each set into raw data, mfcc data, and y data
+# STFT 한 것, CNN 분석하기 위해 Spectogram으로 만든 것, MF한 것, mel0spectogram 한 것
+train_X = []
+train_mfccs = []
+train_y = []
+
+test_X = []
+test_mfccs = []
+test_y = []
+
+# 모든 음성파일의 길이가 같도록 후위에 padding 처리
+pad1d = lambda a, i: a[0: i] if a.shape[0] > i else np.hstack((a, np.zeros(i-a.shape[0])))
+pad2d = lambda a, i: a[:, 0:i] if a.shape[1] > i else np.hstack((a, np.zeros((a.shape[0], i-a.shape[1]))))
+
+frame_length = 0.025
+frame_stride = 0.0010
+
+train_mfccs = [a for (a,b) in trainset]
+train_y = [b for (a,b) in trainset]
+
+test_mfccs = [a for (a,b) in testset]
+test_y = [b for (a,b) in testset]
+
+train_mfccs = np.array(train_mfccs)
+train_y = to_categorical(np.array(train_y))
+
+test_mfccs = np.array(test_mfccs)
+test_y = to_categorical(np.array(test_y))
+
+print('train_mfccs:', train_mfccs.shape)
+print('train_y:', train_y.shape)
+
+print('test_mfccs:', test_mfccs.shape)
+print('test_y:', test_y.shape)
+
+train_X_ex = np.expand_dims(train_mfccs, -1)
+test_X_ex = np.expand_dims(test_mfccs, -1)
+print('train X shape:', train_X_ex.shape)
+print('test X shape:', test_X_ex.shape)
+
+x_train, x_val, y_train, y_val = train_test_split(train_X_ex, train_y,  train_size=0.8, random_state = 66 ) 
+
+
+ip = Input(shape=train_X_ex[0].shape)
+
+m = Conv2D(32, kernel_size=(4,4), activation='relu')(ip)
+m = MaxPooling2D(pool_size=(4,4))(m)
+
+m = Conv2D(32*2, kernel_size=(4,4), activation='relu')(ip)
+m = MaxPooling2D(pool_size=(4,4))(m)
+
+m = Conv2D(32*3, kernel_size=(4,4), activation='relu')(ip)
+m = MaxPooling2D(pool_size=(4,4))(m)
+
+m = Flatten()(m)
+
+m = Dense(64, activation='relu')(m)
+
+m = Dense(32, activation='relu')(m)
+
+op = Dense(3, activation='softmax')(m)
+
+model = Model(ip, op)
+
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+
+eraly_stopping = EarlyStopping(monitor='loss', patience=10, mode='auto') # mode는 min,max,auto 있다
+reLR = ReduceLROnPlateau(patience=5,verbose=1,factor=0.5) #learning rate scheduler
+
+history = model.fit(x_train,
+                    y_train,
+                    epochs=500,
+                    batch_size=8,
+                    verbose=1,
+                    validation_data=(x_val, y_val), callbacks=[eraly_stopping,reLR])
+
+
+model.save('../data/project/file/save_model.h5')
+
+loss, acc= model.evaluate(test_X_ex, test_y, batch_size=8)
+# 지표를 만들기 위한 프레딕트 
+print('loss : ',loss)
+print('acc : ',acc)
+
+
+DATA_DIR = '../data/project/predict/'
+
+Predict_list = ['predict_a','predict_n','predict_s']
+
+
+for p_list in Predict_list:
+
+    wav, sr = librosa.load(DATA_DIR + p_list +'.m4a')
+    mfcc = librosa.feature.mfcc(wav, sr=16000, n_mfcc=100, n_fft=400, hop_length=160)
+    mfcc = sklearn.preprocessing.scale(mfcc, axis=1)
+    padded_mfcc = pad2d(mfcc, 40)
+    padded_mfcc= np.expand_dims(padded_mfcc, 0)
+
+    y_pred = model.predict(padded_mfcc)
+    
+    y_predict=np.argmax(y_pred, axis=1)
+    print('예측값 : ', y_predict)
+
+# 0 - 화남, 1 - 평상시, 2 - 슬픔 
