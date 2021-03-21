af6b55c1da844b6684aa1973b0dc5ba5b5e33fed
bughunt88 <bughunt@naver.com>
2021-03-21 22:04:16 +0900

    2021-03-21

--- Lotte_vision/lotte_ji.py
+++ Lotte_vision/lotte_ji.py
@@ -8,7 +8,7 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import GlobalAveragePooling2D, Flatten, BatchNormalization, Dense, Activation, Dropout, MaxPooling2D
 import pandas as pd
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
-from tensorflow.keras.applications import VGG19, MobileNet, ResNet101, EfficientNetB5
+from tensorflow.keras.applications import VGG19, MobileNet, ResNet101, EfficientNetB5, EfficientNetB7
 from tensorflow.keras.utils import to_categorical
 from tqdm import tqdm
 
@@ -24,34 +24,43 @@ x = preprocess_input(x) # (48000, 255, 255, 3)
 x_pred = preprocess_input(x_pred)   # 
 
 
+#1. 데이터
 idg = ImageDataGenerator(
-    # rotation_range=10, acc 하락
-    width_shift_range=(-1,1),  
-    height_shift_range=(-1,1), 
-    rotation_range=40, 
-    # shear_range=0.2)    # 현상유지
-    zoom_range=0.2,
-    horizontal_flip=True,
-    fill_mode='nearest')
+    validation_split = 0.2,
+    width_shift_range= 0.1,
+    height_shift_range= 0.1,
+    preprocessing_function= preprocess_input,
+    horizontal_flip= True
+)
+
+idg1 = ImageDataGenerator(
+    preprocessing_function= preprocess_input,
+    width_shift_range= 0.05,
+    height_shift_range= 0.05,
+    horizontal_flip= True
+)
 
 idg2 = ImageDataGenerator()
 
 x_train, x_valid, y_train, y_valid = train_test_split(x,y, train_size = 0.9, shuffle = True, random_state=66)
 
-train_generator = idg.flow(x_train,y_train,batch_size=64, seed = 2048)
+train_generator = idg.flow(x_train,y_train,batch_size=16, seed = 2048)
 # seed => random_state
 valid_generator = idg2.flow(x_valid,y_valid)
-# test_generator = idg2.flow(x_pred)
+test_generator = idg1.flow(x_pred)
 
 mc = ModelCheckpoint('../data/lpd_competition/lotte_0317_3.h5',save_best_only=True, verbose=1)
 
 # efficientnet = EfficientNetB4(include_top=False,weights='imagenet',input_shape=x_train.shape[1:])
+
 mobile = EfficientNetB4(include_top=False,weights='imagenet',input_shape=x_train.shape[1:])
 mobile.trainable = True
 a = mobile.output
-a = GlobalAveragePooling2D() (a)
+a = MaxPooling2D() (a)
 a = Flatten() (a)
-a = Dense(4048, activation= 'relu') (a)
+a = Dense(2000, activation= 'swish') (a)
+a = Dropout(0.2) (a)
+a = Dense(1000, activation= 'swish') (a)
 a = Dropout(0.2) (a)
 a = Dense(1000, activation= 'softmax') (a)
 model = Model(inputs = mobile.input, outputs = a)
@@ -63,20 +72,21 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc']
 learning_history = model.fit_generator(train_generator,epochs=200, steps_per_epoch= len(x_train) / 64,
     validation_data=valid_generator, callbacks=[early_stopping,lr,mc])
 
+
 # predict
 from tensorflow.keras.models import Sequential, load_model
 
-model = load_model('../data/lpd_competition/lotte_0317_3.h5')
+# model = load_model('../data/lpd_competition/lotte_0317_3.h5')
 
 tta_steps = 10
 predictions = []
 
 for i in tqdm(range(tta_steps)):
-    preds = model.predict_generator(x_pred,verbose=True)
+    preds = model.predict_generator(test_generator,verbose=True)
     predictions.append(preds)
 
 final_pred = np.mean(predictions, axis=0)
 
 sub = pd.read_csv('../data/lpd_competition/sample.csv')
 sub['prediction'] = np.argmax(final_pred,axis = 1)
-sub.to_csv('../data/lpd_competition/sample_004.csv',index=False)
+sub.to_csv('../data/lpd_competition/sample_007.csv',index=False)
--- Lotte_vision/lotte_vision_dong.py
+++ Lotte_vision/lotte_vision_dong.py
@@ -1,22 +1,33 @@
 import numpy as np
 import pandas as pd
-from tensorflow.keras.applications.efficientnet import EfficientNetB0
+import os
+from tensorflow.keras.applications.efficientnet import EfficientNetB4
 from tensorflow.keras.applications.efficientnet import preprocess_input
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, BatchNormalization, Flatten, Dropout
+from tensorflow.keras.models import Sequential, load_model, Model
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, BatchNormalization, Flatten, Dropout, GlobalAveragePooling2D, Input, GaussianDropout
+from tensorflow.keras.activations import swish
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from scipy import stats
 
 #0. 변수
-batch = 16
+filenum = 13
+batch = 6
 seed = 42
-dropout = 0.2
+dropout = 0.4
 epochs = 1000
-model_path = '../data/LPD_competition/data/lpd_001.hdf5'
-sub = pd.read_csv('../data/LPD_competition/sample.csv', header = 0)
-es = EarlyStopping(patience = 5)
+model_path = load_model('../data/lpd_competition/lotte_0317_3.h5')
+
+save_folder = '../data/lpd_competition/sample_006.csv'
+
+sub = pd.read_csv('../data/lpd_competition/sample.csv', header = 0)
+
+es = EarlyStopping(patience = 7)
 lr = ReduceLROnPlateau(factor = 0.25, patience = 3, verbose = 1)
 cp = ModelCheckpoint(model_path, save_best_only= True)
+
+if not os.path.exists(save_folder):
+    os.mkdir(save_folder)
 
 #1. 데이터
 train_gen = ImageDataGenerator(
@@ -24,19 +35,21 @@ train_gen = ImageDataGenerator(
     width_shift_range= 0.1,
     height_shift_range= 0.1,
     preprocessing_function= preprocess_input,
-    rescale = 1/255.
+    horizontal_flip= True
 )
 
 test_gen = ImageDataGenerator(
     preprocessing_function= preprocess_input,
-    rescale = 1/255.
+    width_shift_range= 0.05,
+    height_shift_range= 0.05,
+    horizontal_flip= True
 )
 
 # Found 39000 images belonging to 1000 classes.
 train_data = train_gen.flow_from_directory(
-    '../data/LPD_competition/train',
-    target_size = (256, 256),
-    class_mode = 'categorical',
+    '../data/lpd/train_new',
+    target_size = (224, 224),
+    class_mode = 'sparse',
     batch_size = batch,
     seed = seed,
     subset = 'training'
@@ -44,9 +57,9 @@ train_data = train_gen.flow_from_directory(
 
 # Found 9000 images belonging to 1000 classes.
 val_data = train_gen.flow_from_directory(
-    '../data/LPD_competition/train',
-    target_size = (256, 256),
-    class_mode = 'categorical',
+    '../data/lpd/train_new',
+    target_size = (224, 224),
+    class_mode = 'sparse',
     batch_size = batch,
     seed = seed,
     subset = 'validation'
@@ -54,40 +67,73 @@ val_data = train_gen.flow_from_directory(
 
 # Found 72000 images belonging to 1 classes.
 test_data = test_gen.flow_from_directory(
-    '../data/LPD_competition/test',
-    target_size = (256, 256),
+    '../data/lpd/test_new',
+    target_size = (224, 224),
     class_mode = None,
     batch_size = batch,
-    seed = seed,
     shuffle = False
 )
 
 #2. 모델
-eff = EfficientNetB0(include_top = False, input_shape = (256, 256, 3))
-eff.trainable = False
+eff = EfficientNetB4(include_top = False, input_shape=(224, 224, 3))
+eff.trainable = True
 
-model = Sequential()
-model.add(eff)
-model.add(MaxPooling2D(2, padding = 'same'))
-model.add(Conv2D(1280, 2, padding = 'same'))
-model.add(MaxPooling2D(2, padding = 'same'))
-model.add(BatchNormalization())
-model.add(Flatten())
-model.add(Dense(5120, activation = 'relu', kernel_initializer = 'he_normal'))
-model.add(Dropout(dropout))
-model.add(Dense(2560, activation = 'relu', kernel_initializer = 'he_normal'))
-model.add(Dense(1280, activation = 'relu', kernel_initializer = 'he_normal'))
-model.add(Dense(1000, activation = 'softmax'))
+pretrained = eff.output
+layer_1 = Flatten()(pretrained)
+layer_2 = Dense(2000, activation = 'relu')(layer_1)
+layer_2 = Dropout(dropout)(layer_2)
+layer_3 = Dense(1000, activation = 'relu')(layer_2)
+output = Dense(1000, activation = 'softmax')(layer_3)
+
+model = Model(inputs = eff.input, outputs = output)
 
 #3. 컴파일 훈련
-model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['acc'])
-model.fit(train_data, steps_per_epoch = len(train_data), validation_data= val_data, validation_steps= len(val_data),\
+model.compile(loss = 'sparse_categorical_crossentropy', optimizer = 'adam', metrics = ['sparse_categorical_accuracy'])
+model.fit(train_data, steps_per_epoch = np.ceil(39000/batch), validation_data= val_data, validation_steps= np.ceil(9000/batch),\
     epochs = epochs, callbacks = [es, cp, lr])
 
 model = load_model(model_path)
 
 #4. 평가 예측
-pred = model.predict(test_data)
-pred = np.argmax(pred, 1)
-sub.loc[:,'prediction'] = pred
-sub.to_csv('../data/LPD_competition/sample_001.csv', index = False)
+result = []
+'''
+for tta in range(50):
+    print(f'{tta+1} 번째 TTA 진행중 - mode')
+    pred = model.predict(test_data, steps = len(test_data))
+    pred = np.argmax(pred, 1)
+    result.append(pred)
+    print(f'{tta+1} 번째 제출 파일 저장하는 중')
+    temp = np.array(result)
+    temp = np.transpose(result)
+    temp_mode = stats.mode(temp, axis = 1).mode
+    sub.loc[:, 'prediction'] = temp_mode
+    sub.to_csv(save_folder + '/sample_{0:03}_{1:02}.csv'.format(filenum, (tta+1)), index = False)
+    temp_count = stats.mode(temp, axis = 1).count
+    for i, count in enumerate(temp_count):
+        if count < tta/2.:
+            print(f'{tta+1} 반복 중 {i} 번째는 횟수가 {count} 로 {(tta+1)/2.} 미만!')
+'''
+cumsum = np.zeros([72000, 1000])
+for tta in range(50):
+    print(f'{tta+1} 번째 TTA 진행중 - TTA')
+    pred = model.predict(test_data, steps = len(test_data)) # (72000, 1000)
+    
+    cumsum = np.add(cumsum, pred)
+    temp = cumsum / (tta+1)
+    temp_sub = np.argmax(temp, 1)
+    temp_percent = np.max(temp, 1)
+    
+    count = 0
+    print(f'TTA {tta} : {count} 개가 불확실!')
+    i = 0
+    for percent in temp_percent:
+        count = 0
+        if percent < 0.3:
+            print(f'{i} 번째 테스트 이미지는 {percent}% 의 정확도를 가짐')
+            count += 1
+        i += 1
+    print(f'{tta+1} ')
+    print(f'{tta+1} 번째 제출 파일 저장하는 중')
+    sub.loc[:, 'prediction'] = temp_sub
+    sub.to_csv(save_folder + '/sample_{0:03}_{1:02}.csv'.format(filenum, (tta+1)), index = False)
+    
