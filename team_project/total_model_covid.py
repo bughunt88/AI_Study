c20724a989eeca1c5b932610441f1d9c4fd6107b
hansoo kim <bughunt88@gmail.com>
2021-05-20 17:10:47 +0900

    2021-05-20

---
+++ team_project/total_model_covid.py
@@ -0,0 +1,179 @@
+import numpy as np
+import db_connect as db
+import pandas as pd
+import timeit
+import tensorflow as tf
+import numpy as np
+
+from tensorflow.keras.layers import Conv1D, MaxPooling1D, Dense, Flatten, Dropout,LSTM, Conv2D,Input,Activation, LSTM, Dropout, BatchNormalization
+from tensorflow.keras.models import Sequential, Model, load_model
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split, KFold
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.optimizers import Adam, Adadelta, Adamax, Adagrad
+from tensorflow.keras.optimizers import RMSprop, SGD, Nadam
+from tensorflow.keras.activations import tanh, relu, elu, selu, swish
+from sklearn.metrics import mean_absolute_error as mae_
+from sklearn.metrics import mean_squared_error as mse_
+import tensorflow.keras.backend as K
+
+# db 직접 불러오기 
+fold_score_list = []
+history_list=[]
+csv_list = []
+
+def RMSE(y_test, y_predict): 
+    return np.sqrt(mean_squared_error(y_test, y_predict)) 
+
+def mish(x):
+    return x * K.tanh(K.softplus(x))
+
+def callbacks(modelpath):
+    er = EarlyStopping(monitor = 'val_loss',patience=20)
+    mo = ModelCheckpoint(monitor = 'val_loss',filepath = modelpath,save_best_only=True, verbose=1)
+    lr = ReduceLROnPlateau(monitor = 'val_loss',patience=10, factor=0.3, verbose=1)
+    return er,mo,lr
+
+def build_model(acti, opti, lr):   
+
+    # 2. 모델구성
+    
+    inputs = Input(shape=(x_train.shape[1]),name='input')
+
+    x = Dense(32,activation="relu")(inputs)
+    x = BatchNormalization()(x)
+    x = Dense(32,activation="relu")(x)
+    x = Dense(32,activation="relu")(x)
+    x = Dense(32,activation="relu")(x)
+    x = Dense(16,activation="relu")(x)
+    x = Dense(16,activation="relu")(x)
+
+    outputs = Dense(1)(x)
+    model = Model(inputs=inputs,outputs=outputs)
+
+    # 3. 컴파일 훈련        
+    model.compile(loss='mse', optimizer = opti(learning_rate=lr), metrics='mae')
+
+    return model
+
+def evaluate_list(model):
+    score_list = []
+    #4. 평가, 예측
+    y_predict = model.predict(x_pred)
+
+    # r2_list
+    r2 = r2_score(y_pred, y_predict)
+    score_list.append(r2)
+    print('r2 : ', r2)
+    # rmse_list
+    rmse = mse_(y_pred, y_predict, squared=False)
+    score_list.append(rmse)
+    print('rmse : ', rmse)
+    # mae_list
+    mae = mae_(y_pred, y_predict)
+    score_list.append(mae)
+    print('mae : ', mae)
+    # mse_list
+    mse = mse_(y_pred, y_predict, squared=True)
+    score_list.append(mse)
+    print('mse : ', mse)
+
+    return  score_list, y_predict
+
+start_time = timeit.default_timer()
+
+
+def load_data(query, is_train = True):
+    query = query
+    db.cur.execute(query)
+    dataset = np.array(db.cur.fetchall())
+
+    # pandas 넣기
+    column_name = ['date', 'year', 'month', 'day', 'time', 'category', 'dong','temperature','rain','wind','humidity','person', 'value']
+    df = pd.DataFrame(dataset, columns=column_name)
+    db.connect.commit()
+
+    # pred = df.iloc[:,1:-1]
+
+    if is_train == True:
+        # train, test 나누기
+        train_value = df[ '2020-09-01' > df['date'] ]
+        x = train_value.iloc[:,1:-1].astype('float64')
+        y = train_value.iloc[:,-1].astype('float64').to_numpy()
+    else:
+        test_value = df[df['date'] >=  '2020-09-01']
+        x = test_value.iloc[:,1:-1].astype('float64')
+        y = test_value.iloc[:,-1].astype('float64').to_numpy()
+
+    
+    # 원 핫으로 컬럼 추가해주는 코드!!!!!    
+    x = pd.get_dummies(x, columns=["category", "dong"]).to_numpy()
+    # 카테고리랑 동만 원핫으로 해준다 
+
+    return x, y
+
+x_pred, y_pred = load_data("SELECT d.date,YEAR,MONTH, d.day, d.time, category, dong,temperature,rain,wind,humidity, IFNULL( c_person,0) AS person, VALUE FROM main_data_table AS d INNER JOIN `weather` AS s ON d.date = s.DATE AND d.time = s.time LEFT JOIN `covid19_re` AS c ON c.date = d.date ", is_train = False)
+x_train, y_train = load_data("SELECT d.date,YEAR,MONTH, d.day, d.time, category, dong,temperature,rain,wind,humidity, IFNULL( c_person,0) AS person, VALUE FROM main_data_table AS d INNER JOIN `weather` AS s ON d.date = s.DATE AND d.time = s.time LEFT JOIN `covid19_re` AS c ON c.date = d.date WHERE (d.TIME != 2 AND d.TIME != 3 AND d.TIME != 4 AND d.TIME != 5  AND d.TIME != 6 AND d.TIME != 7 AND d.TIME != 8) ORDER BY DATE, YEAR, MONTH, DAY, TIME, category, dong ASC")
+
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1],1)
+x_pred = x_pred.reshape(x_pred.shape[0], x_pred.shape[1],1)
+
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train,  train_size=0.9, random_state = 77, shuffle=True ) 
+
+print(x_train.shape, y_train.shape) #(2213481, 46, 1) (2213481,)
+print(x_pred.shape, y_pred.shape)   #(177408, 46, 1) (177408,)
+print(x_val.shape, y_val.shape)     #(245943, 46, 1) (245943,) 
+
+leaky_relu = tf.nn.leaky_relu
+acti_list = [leaky_relu, mish, 'swish', 'elu', 'relu', 'selu','tanh']
+opti_list = [RMSprop, Nadam, Adam, Adadelta, Adamax, Adagrad]
+batch = 500
+lrr = 0.001
+epo = 100
+for op_idx,opti in enumerate(opti_list):
+    for ac_idx,acti in enumerate(acti_list):
+        modelpath = f'../data/modelcheckpoint/15_models_covid_{op_idx}_{ac_idx}.hdf5'
+        model = build_model(acti, opti, lrr)
+        # model = load_model(modelpath, custom_objects={'leaky_relu':tf.nn.leaky_relu, 'mish':mish})
+
+        # 훈련
+        er,mo,lr = callbacks(modelpath) 
+        history = model.fit(x_train, y_train, verbose=1, batch_size=batch, epochs = epo, validation_data=(x_val,y_val), callbacks = [er, lr, mo])
+        # history_list.append(history)
+
+        score, y_predict = evaluate_list(model)
+        # 엑셀 추가 코드 
+        # 경로 변경 필요!!!!
+        df = pd.DataFrame(y_pred)
+        df[f'{op_idx}_{ac_idx}'] = y_predict
+        df.to_csv(f'../data/modelcheckpoint/15_models_covid_{op_idx}_{ac_idx}.csv',index=False)
+
+        fold_score_list.append(score)
+        print('=============parameter=================')
+        print('optimizer : ', opti, '\n activation : ', acti, '\n batch_size : ', batch, '\n lr : ', lrr, '\n epochs : ', epo)
+        print('r2   : ', score[0])
+        print('rmse : ', score[1])
+        print('mae : ', score[2])
+        print('mse : ', score[3])
+        print(f'======================================')
+
+
+terminate_time = timeit.default_timer() # 종료 시간 체크  
+print("%f초 걸렸습니다." % (terminate_time - start_time))
+
+print('=========================final score========================')
+print("r2           rmse          mae            mse: ")
+fold_score_list = np.array(fold_score_list).reshape(len(opti_list),len(acti_list),4)
+print(fold_score_list)
+'''
+optimizer :  <class 'tensorflow.python.keras.optimizer_v2.rmsprop.RMSprop'> 
+ activation :  tanh
+ batch_size :  500
+ lr :  0.001
+ epochs :  5000
+r2   :  0.858529394567721
+rmse :  1.459906920570742
+mae :  0.3961494602692392
+mse :  2.131328216730347
+'''
